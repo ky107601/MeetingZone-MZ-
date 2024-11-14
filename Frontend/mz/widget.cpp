49395699060090ed014795f30cf342037ab23ba2
ky107601 <ky107601@github.com>
2024-11-14 16:02:04 +0900

    Add: Page layout

--- Frontend/mz/widget.cpp
+++ Frontend/mz/widget.cpp
@@ -1,11 +1,30 @@
 #include "widget.h"
 #include "./ui_widget.h"
 
+#include <QGraphicsDropShadowEffect> //for shadow effect
+
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    /* Set the inputBox style */
+    QGraphicsDropShadowEffect *shadowEffect = new QGraphicsDropShadowEffect();
+    shadowEffect->setBlurRadius(5);         // Set blur radius for the shadow
+    shadowEffect->setOffset(5, 5);           // Set shadow offset (x, y)
+    shadowEffect->setColor(Qt::gray);       // Set shadow color
+    ui->inputBox->setGraphicsEffect(shadowEffect);
+
+    QWidget *page2 = new QWidget;
+    ui->stackedWidget->addWidget(page2);
+
+    connect(ui->enterBtn, &QPushButton::clicked, this, [&](){
+        ui->stackedWidget->setCurrentIndex(1);
+    });
+
+    //ui->chatInput->setAlignment(Qt::AlignVCenter);
+    ui->chatInput->setPlaceholderText("Enter your text here ...");
 }
 
 Widget::~Widget()
