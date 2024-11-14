11be845c8441a246cec714749f8e7401f0ef4c09
ky107601 <ky107601@github.com>
2024-11-14 11:43:28 +0900

    Add: Qt project added

---
+++ Frontend/mz/build/Desktop_Qt_6_7_3-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_mz_FILE /home/pi/mz/build/Desktop_Qt_6_7_3-Debug/mz)
+set(__QT_DEPLOY_TARGET_mz_TYPE EXECUTABLE)
---
+++ Frontend/mz/build/Desktop_Qt_6_7_3-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/pi/mz/build/Desktop_Qt_6_7_3-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/home/pi/Qt/6.7.3/gcc_arm64/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/gcc-12" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/pi/Qt/6.7.3/gcc_arm64" CACHE "PATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/aarch64-linux-gnu-g++" CACHE "FILEPATH" "" FORCE)
