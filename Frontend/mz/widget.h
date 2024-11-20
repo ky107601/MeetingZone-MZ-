765976aaae11bd8ae2c2a1f7e6ed94b978999440
ky107601 <ky107601@github.com>
2024-11-20 11:56:04 +0900

    Feat: video streaming

--- Frontend/mz/widget.h
+++ Frontend/mz/widget.h
@@ -2,6 +2,18 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QIcon>
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/videoio.hpp>
+#include <opencv4/opencv2/core/mat.hpp>
+#include <QImage>
+#include <QPixmap>
+#include <QTimer>
+#include "camerastreamer.h"
+
+using namespace cv;
+using namespace std;
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +29,22 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+    bool micFlag = false; //mic status
+    QIcon micOffIcon; //mic icon
+    QIcon micOnIcon;
+
+    bool videoFlag = false; //video status
+    QIcon videoOffIcon; //video icon
+    QIcon videoOnIcon;
+
+    VideoCapture cap;
+
+public slots:
+    void changeIcon();
+    void updateFrame();
+
 private:
     Ui::Widget *ui;
+
 };
 #endif // WIDGET_H
