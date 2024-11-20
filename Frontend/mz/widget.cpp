e7598aacd0bdb11c4f3d04b1e25704a1f81b1cdf
ky107601 <ky107601@github.com>
2024-11-20 14:44:25 +0900

    edited frame size

--- Frontend/mz/widget.cpp
+++ Frontend/mz/widget.cpp
@@ -66,7 +66,7 @@ Widget::Widget(QWidget *parent)
 
     string pipeline =
         "libcamerasrc camera-name=/base/axi/pcie@120000/rp1/i2c@88000/ov5647@36 "
-        "! video/x-raw,width=640,height=480,framerate=10/1,format=RGBx "
+        "! video/x-raw,width=1280,height=720,framerate=10/1,format=RGBx "
         "! videoconvert ! videoscale ! appsink";
 
     if(!cap.open(pipeline, CAP_GSTREAMER))
@@ -112,7 +112,8 @@ void Widget::updateFrame()
 
         cvtColor(frame, frame, COLOR_BGR2RGB); // Convert to RGB format
         QImage qimg(frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888);
-        ui->videoLabel->setFixedSize(frame.cols, frame.rows);
+        qDebug()<<"cols = "<<frame.cols<<"rows = "<<frame.rows;
+        ui->videoLabel->setFixedSize(frame.cols/5, frame.rows/5);
         ui->videoLabel->setPixmap(QPixmap::fromImage(qimg).scaled(ui->videoLabel->size(), Qt::KeepAspectRatio));
     }
 
