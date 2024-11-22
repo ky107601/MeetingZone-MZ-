d541eced52d6f8421d1b21dc410f9f0bee5179ff
kshoo0421 <kshoo0421@gmail.com>
2024-11-22 15:11:42 +0900

    opencv static lib build

--- Frontend/mz/networkmanager.h
+++ Frontend/mz/networkmanager.h
@@ -1,24 +1,17 @@
 #ifndef NETWORKMANAGER_H
 #define NETWORKMANAGER_H
-#include <QObject>
-#include <QNetworkAccessManager>
-#include <QLineEdit>
-#include <QTimer>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
 
-class NetworkManager : public QWidget
+class NetworkManager 
 {
-    Q_OBJECT
 public:
-    NetworkManager(QWidget *parent = nullptr);
+    NetworkManager();
     ~NetworkManager();
 
     void sendMetadata(); //send data to server
     void startStreaming(); //start rtp/rtsp
-public slots:
-
 private:
 
 };
