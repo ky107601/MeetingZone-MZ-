10e43ddc0ab31e8745b7498a2ac20e6a1ea0b35a
kshoo0421 <shmd02@gmail.com>
2024-12-04 14:27:19 +0900

    static -> dtnamic

--- Frontend/mz/networkmanager.h
+++ Frontend/mz/networkmanager.h
@@ -1,19 +1,26 @@
 #ifndef NETWORKMANAGER_H
 #define NETWORKMANAGER_H
+
+#include <iostream>
+#include <thread>
+#include <cstdlib>
+#include <csignal>
+#include <chrono>
+#include <string>
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
+extern "C" {
+    #include <ifaddrs.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
 
-class NetworkManager 
-{
-public:
-    NetworkManager();
-    ~NetworkManager();
-
-    void sendMetadata(); //send data to server
-    void startStreaming(); //start rtp/rtsp
-private:
-
-};
-
+    #include <libavformat/avformat.h>
+    #include <libavcodec/avcodec.h>
+    #include <libavutil/avutil.h>
+    #include <libavutil/imgutils.h>
+    #include <libavdevice/avdevice.h>
+    #include <libswscale/swscale.h>
+    #include <libavutil/time.h>
+}
 #endif // NETWORKMANAGER_H
